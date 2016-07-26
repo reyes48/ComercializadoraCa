d26a82196a2f42f5a01f3a99580e8bbb37622892
inEdgar <edgar.psda@gmail.com>
2016-07-26 09:27:42 -0700

    Primer Commmit
    Se crea el proyecto en git subiendo el template a utilizar

---
+++ fronteed.com/iCheck/icheckf700.js
@@ -0,0 +1,14 @@
+<HTML>
+<!-- Created by HTTrack Website Copier/3.48-21 [XR&CO'2014] -->
+
+<!-- Mirrored from fronteed.com/iCheck/icheck.js?v=1.0.1 by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 03 Mar 2016 00:30:22 GMT -->
+<HEAD>
+<META HTTP-EQUIV="Content-Type" CONTENT="text/html;charset=UTF-8"><META HTTP-EQUIV="Refresh" CONTENT="0; URL=http://fronteed.com/?v=1.0.1"><TITLE>Page has moved</TITLE>
+</HEAD>
+<BODY>
+<A HREF="http://fronteed.com/?v=1.0.1"><h3>Click here...</h3></A>
+</BODY>
+<!-- Created by HTTrack Website Copier/3.48-21 [XR&CO'2014] -->
+
+<!-- Mirrored from fronteed.com/iCheck/icheck.js?v=1.0.1 by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 03 Mar 2016 00:30:22 GMT -->
+</HTML>
